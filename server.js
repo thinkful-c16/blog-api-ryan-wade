adb4f0623e958c3b507de37183efd00653b51262
Owcollier <owcollier@gmail.com>
2017-11-15 15:32:56 -0500

    installed express, morgan, fleshed out server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// const blogPostRouter = require('./blogPostRouter');
+app.use(morgan('common'));
+
+// app.use('/blog-posts', blogPostRouter);
+
+app.get('/', (req, res) => {
+  console.log('Your get request worked');
+  res.json('hey');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
